aa9f72d8d2236f817ee5d6ad5cf6c00ecca4d5a9
Audrea Cook <audreacwork@gmail.com>
2019-07-15 00:44:20 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -29,8 +29,9 @@ def apply_coupons(cart, coupons)
       # if cart has the item and :count = :num, add item with adjusted price
       if cart[key] 
         new_item = "#{key} W/COUPON"
-        cart[new_item][:count] = coupons[:num]
-        cart[new_item][:price] = coupons[:cost] / coupons[:num]
+        cart[new_item]
+        #cart[new_item][:count] = coupons[:num]
+        #cart[new_item][:price] = coupons[:cost] / coupons[:num]
       # if cart has item and :count > :num, add item and adjust count
       binding.pry
       end
